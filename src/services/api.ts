9ba11e3aa56831e776613b2f083701ed93163945
Albiere-Lima <albieregames30@gmail.com>
2026-09-17 14:06:04 -0300

    1.0.0-BETA

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://backendcalcc.internal/api',
 });
 
 api.interceptors.request.use((config) => {
